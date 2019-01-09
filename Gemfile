d94bcf31c40664cdc4db867f9265f2d857a16752
Plamen Mitev <pmitev89@gmail.com>
2019-01-09 00:07:20 +0000

    added content

--- Gemfile
+++ Gemfile
@@ -12,6 +12,7 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'bootstrap-sass', '~> 3.4'
+gem 'faker', '~> 1.9', '>= 1.9.1'
 
 group :development, :test do
   gem 'byebug'
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,43 @@
 class HomeController < ApplicationController
+  before_action :common_content, :only => [:index, :team]
+  
+  def common_content
+    @hero_name = Faker::Superhero.name
+    @hero_power = Faker::Superhero.power
+    @hero_prefix = Faker::Superhero.prefix
+    @corp_power = Faker::Company.bs
+    @corp_power = @corp_power.titleize
+    @origins = Faker::Nation.nationality
+    
+    @ran_num = rand(1..1000)
+    
+    @set_num = rand(1..2)
+    @bg = rand(1..2)
+    
+    #Randomly pick an avatar set
+    if @set_num == 1
+      @set = "set1"
+    else 
+      @set = "set2"
+    end
+    
+    #Randomly pick an background set
+    if @bg == 1
+      @back = "bg1"
+    else 
+      @back = "bg2"
+    end
+
+    @avatar = Faker::Avatar.image(@ran_num, "300x300", "png", @set, @back)
+  end
+  
+  
   def index
   end
   
   def team
   end
+  
+  
   
 end
